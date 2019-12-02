a9498fad9f81e7acb2ec1a83aa36c5ce16bf0963
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2019-12-02 08:26:38 -0600

    first commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../boriz.zip');
+var kuduApi = 'https://boriz.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$boriz';
+var password = 'i14St6fi99bDrY8S0g5fGaBd39FPLSQtslYisya1eANAFhJjBQYig0Cn1C9x';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('boriz publish');
+  } else {
+    console.error('failed to publish boriz', err);
+  }
+});
